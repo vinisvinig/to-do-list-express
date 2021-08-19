fcb7dccfaf06945745a278c54bfc1cd5dcfcc443
Vinícius Souza Girardi <vini.girardi14@gmail.com>
2021-08-18 23:22:06 -0300

    Atualização com comandos

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const path = require('path')
 
 const checkListRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
+const methodOverride = require('method-override')
 
 require('./config/database');
 
 
 const app = express();
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method', { methods: ['POST', 'GET']}))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
